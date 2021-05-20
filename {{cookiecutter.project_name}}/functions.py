932687fcef600011996dbe14da0e2808e76062ad
yuu-eguci <yuu.eguci@gmail.com>
2021-05-20 21:48:21 +0900

    ✨ functions.py

---
+++ {{cookiecutter.project_name}}/functions.py
@@ -0,0 +1,14 @@
+"""{{cookiecutter.project_name}} random function module
+プロジェクト序盤にざくざく実装するためのモジュールです。
+大きくなりすぎたときは、のちに整理します。
+"""
+
+# Built-in modules.
+
+# Third-party modules.
+
+# User modules.
+
+
+if __name__ == '__main__':
+    pass
