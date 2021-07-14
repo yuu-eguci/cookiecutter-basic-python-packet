da71e98a5202915bd3255563cf0f048c4303151a
yuu-eguci <yuu.eguci@gmail.com>
2021-07-14 17:49:59 +0900

    ✨ utils.DbClient

--- {{cookiecutter.project_name}}/utils.py
+++ {{cookiecutter.project_name}}/utils.py
@@ -11,6 +11,7 @@ import datetime
 import pytz
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+import mysql.connector
 
 # User modules.
 import consts
@@ -77,6 +78,73 @@ def send_slack_message(message: str) -> None:
         # str like 'invalid_auth', 'channel_not_found'
         assert e.response['error']
         logger.error(f'Got an error: {e.response["error"]}')
+
+
+class DbClient:
+    """DB アクセスを行うクラスです。 with 構文で使用可能です。
+    with utils.DbClient() as db_client:
+        records = db_client.sample_select()
+    """
+
+    def __enter__(self):
+        mysql_connection_config = {
+            'host': consts.MYSQL_HOST,
+            'user': consts.MYSQL_USER,
+            'password': consts.MYSQL_PASSWORD,
+            'database': consts.MYSQL_DATABASE,
+        }
+        self.connection = mysql.connector.connect(**mysql_connection_config)
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.connection.close()
+
+    def sample_select(self) -> list:
+        """サンプルです。
+        Returns:
+            list: Select 結果のリスト。
+        """
+
+        select_sql = ' '.join([
+            'SELECT',
+                'id',  # noqa: E131
+            'FROM sampletable',
+            'WHERE',
+                'id = %s',
+            'ORDER BY id DESC',
+        ])
+        cursor = self.connection.cursor(dictionary=True)
+        cursor.execute(select_sql, (1,))
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+    def sample_update(self):
+        """サンプルです。
+        """
+
+        update_sql = ' '.join([
+            'UPDATE sampletable',
+            'SET',
+                'foo = %s',  # noqa: E131
+            'WHERE id = %s',
+        ])
+        cursor = self.connection.cursor()
+        cursor.execute(update_sql, (1, 1))
+        cursor.close()
+        self.connection.commit()
+
+
+def get_placeholder(count: int) -> str:
+    """count ぶんのプレースホルダ文字列を作ります。
+    %s, %s, %s, %s, ...
+    Args:
+        count (int): 欲しい %s の数。
+    Returns:
+        str: %s, %s, %s, %s, ...
+    """
+
+    return ','.join(('%s' for i in range(count)))
 
 
 # utils モジュール用のロガーを作成します。
